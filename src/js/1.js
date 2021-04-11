364ecf573b8edfe7b2de233b33ffa39ea794d4a9
a <amanatman@gmail.com>
2021-04-11 06:05:11 +0530

    model

--- src/js/1.js
+++ src/js/1.js
@@ -1,3 +1,21 @@
 function init() {
     console.log("Using thee ver" + THREE.REVISION)
 }
+
+const loader = new GLTFLoader();
+loader.load( './star.glb', function ( gltf ) {
+
+    let model = gltf.scene;
+    model.rotation.y = 0.5 * Math.PI;
+    model.rotation.x = 0.5 * Math.PI;
+
+    model.rotation.y = 0.5 * Math.PI;
+    model.name = 'star';
+    scene.add( model );
+    // console.log
+
+    }, undefined, function ( error ) {
+
+    console.error( error );
+
+    } );
